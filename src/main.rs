b561d2006713f255dbe42858f2f01038440e77f5
lingxiang.wang <lingxiang.wang@bytedance.com>
2020-12-19 14:47:28 -0500

    VGA BUFFER DONE

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,18 @@
 #![no_main]
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World! hahahahah";
+mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x9
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
