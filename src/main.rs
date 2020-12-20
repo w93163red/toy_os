a3d3acefed470e89cba3ee9c0d41cd8eb6cb62b0
lingxiang.wang <lingxiang.wang@bytedance.com>
2020-12-20 10:41:05 -0500

    test failed code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 use core::panic::PanicInfo;
 
 mod vga_buffer;
@@ -16,4 +18,12 @@ pub extern "C" fn _start() -> ! {
     panic!("Some panic message");
 
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
